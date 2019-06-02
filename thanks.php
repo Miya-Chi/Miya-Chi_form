9231237038e530876fabebd84cb1067a45cffa53
みやしたちひろ <c09069354869@gmail.com>
2019-06-02 22:08:21 +0800

    thanks 作成

--- thanks.php
+++ thanks.php
@@ -1,23 +1,35 @@
-<?php
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+ <?php
+    if($_SERVER['REQUEST_METHOD'] !== 'POST'){
         header('Location: index.html');
     }
-    
-    $nickname = $_POST['nickname'];
-    $email = $_POST['email'];
-    $content = $_POST['content'];
- ?>
- <!DOCTYPE html>
- <html lang="ja">
- <head>
-  <meta charset="utf-8">
-   <title></title>
- </head>
- <body>
-  <h1>お問い合わせありがとうございました！</h1>
-  <p><?php echo $nickname ?></p>
-  <p><?php echo $email ?></p>
-  <p><?php echo $content ?></p>
+    // if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    //     header('Location: index.html');
+    // }
+    // 関数の呼び出し
+    require_once('function.php');
+    $nickname = h($_POST['nickname']);
+    $email = h($_POST['email']);
+    $content = h($_POST['content']);
+    require_once('db_connect.php');
+    $stmt = $dbh->prepare('INSERT INTO surveys (nickname, email, content) VALUES (?, ?, ?)');
+    $stmt->execute([$nickname, $email, $content]);//?を変数に置き換えてSQLを実行
 
- </body>
- </html>
+    // DBとの接続
+    require_once('db_connect.php');
+    $stmt = $dbh->prepare('INSERT INTO surveys (nickname,email,content) VALUES(?,?,?)');
+    $stmt->execute([$nickname, $email, $content]);
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>送信完了</title>
+    </head>
+    <body>
+        <h1>お問い合わせありがとうございました！</h1>
+        <p><?php echo $nickname ?></p>
+        <p><?php echo $email ?></p>
+        <p><?php echo $content ?></p>
+    </body>
+</html>

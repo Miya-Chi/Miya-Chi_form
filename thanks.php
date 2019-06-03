eb59625b180f8fae53a1172797e7f36368a16b65
みやしたちひろ <c09069354869@gmail.com>
2019-06-03 08:36:31 +0800

    戻るボタン追加

--- thanks.php
+++ thanks.php
@@ -24,12 +24,19 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <title>送信完了</title>
+        <title>サンクスページ</title>
+        <link href="style.css" rel="stylesheet">
+        <link href="bootstrap.css" rel="stylesheet">
     </head>
     <body>
-        <h1>お問い合わせありがとうございました！</h1>
+      <div id="contact" class="font-weight-light">
+        <h1 class="font-weight-light">♡THX♡</h1>
         <p><?php echo $nickname ?></p>
         <p><?php echo $email ?></p>
         <p><?php echo $content ?></p>
+        <a href="index.html">
+        <input type="button" value="once more" class="btn btn-info">
+        </a>
+      </div>
     </body>
 </html>

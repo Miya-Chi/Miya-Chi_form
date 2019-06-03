7cd17e8def526f98d28de933dc82bc2cd8ea7a78
みやしたちひろ <c09069354869@gmail.com>
2019-06-03 08:45:08 +0800

    装飾追加

--- check.php
+++ check.php
@@ -37,8 +37,11 @@
 <head>
   <meta charset="utf-8">
   <title>入力内容確認</title>
+  <link href="style.css" rel="stylesheet">
+  <link href="bootstrap.css" rel="stylesheet">
 </head>
 <body>
+  <div id="contact" class="font-weight-light">
     <h1>入力内容確認</h1>
     <p><?php echo $nickname_result ?></p>
     <p><?php echo $email_result ?></p>
@@ -47,12 +50,13 @@
       <input type="hidden" name="nickname" value="<?php echo $nickname ?>">
       <input type="hidden" name="email" value="<?php echo $email ?>">
       <input type="hidden" name="content" value="<?php echo $content?>">
-      <input type="button" value="戻る" onclick="history.back()">
+      <input type="button" value="Back" onclick="history.back()" class="btn btn-info">
       <?php if ($email != '' && $nickname != '' && $content != ''):
         // コロン構文
          ?>
-      <input type="submit" value="OK">
+      <input type="submit" value="OK" class="btn btn-info">
     <?php endif;?>
     </form>
+  </div>
 </body>
 </html>
--- list.php
+++ list.php
@@ -13,12 +13,20 @@
   <head>
     <meta  charset="UTF-8">
     <title>Document</title>
+    <link href="style.css" rel="stylesheet">
+    <link href="bootstrap.css" rel="stylesheet">
   </head>
   <body>
+    <div id="contact" class="font-weight-light">
+    <a href="index.html">
+      <input type="button" value="list" class="btn btn-info">
+    </a>
   <?php foreach ($results as $result): ?>
-    <p><?php echo h($result['nickname']) ?></p>
-    <p><?php echo h($result['email']) ?></p>
-    <p><?php echo h($result['content']) ?></p>
-<?php endforeach ?>
+    <p>name♡<?php echo h($result['nickname']) ?></p>
+    <p>email♡<?php echo h($result['email']) ?></p>
+    <p>contact♡<?php echo h($result['content']) ?></p>
+    <hr>
+    <?php endforeach ?>
+  </div>
   </body>
   </html>
